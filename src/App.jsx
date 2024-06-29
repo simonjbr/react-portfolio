50cea02ac4c62064fe5e7d08b9083655fb461d4c
Simon Brown <simonandon@live.com.au>
2024-06-29 12:32:35 +0800

    Removed unused imports and changed page title icon

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
 import './App.css';
 
 import { Outlet } from 'react-router-dom';
 
 import Header from './components/Header';
-import About from './pages/About';
-import Portfolio from './pages/Portfolio';
-import Contact from './pages/Contact';
-import Resume from './pages/Resume';
+// import About from './pages/About';
+// import Portfolio from './pages/Portfolio';
+// import Contact from './pages/Contact';
+// import Resume from './pages/Resume';
 
 function App() {
 	return (
