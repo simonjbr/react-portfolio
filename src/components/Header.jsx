9ac54b0c4712c1e033c692be3d52cdb0230a9685
Simon Brown <simonandon@live.com.au>
2024-06-29 13:03:47 +0800

    Changed a tags to Link tags and added some conditional classes for styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,82 +1,119 @@
+import { Link, useLocation } from 'react-router-dom';
 import { GiHamburgerMenu } from 'react-icons/gi';
 
 export default function Header() {
+	const currentPage = useLocation().pathname;
+
 	return (
 		<>
 			<div className="px-5 navbar bg-gradient-to-tr from-slate-800 to-blue-500">
 				<div className="navbar-start">
 					<h1 className="text-5xl font-bold text-gray-350 hover:text-gray-500 w-auto flex align-middle transition-all">
-						<a href="/">simonjbr</a>
+						<Link to="/">simonjbr</Link>
 					</h1>
 				</div>
 				<div className="navbar-end">
 					<ul className="text-3xl transition-all flex justify-between max-sm:hidden">
 						<li>
-							<a className="btn-ghost rounded-md p-2" href="/">
+							<Link
+								className={`btn-ghost rounded-md p-2 ${
+									currentPage === '/' &&
+									'underline text-gray-300 font-medium'
+								}`}
+								to="/"
+							>
 								About
-							</a>
+							</Link>
 						</li>
 						<li>
-							<a
-								className="btn-ghost rounded-md p-2"
-								href="/Portfolio"
+							<Link
+								className={`btn-ghost rounded-md p-2 ${
+									currentPage === '/Portfolio' &&
+									'underline text-gray-300 font-medium'
+								}`}
+								to="/Portfolio"
 							>
 								Portfolio
-							</a>
+							</Link>
 						</li>
 						<li>
-							<a
-								className="btn-ghost rounded-md p-2"
-								href="/Contact"
+							<Link
+								className={`btn-ghost rounded-md p-2 ${
+									currentPage === '/Contact' &&
+									'underline text-gray-300 font-medium'
+								}`}
+								to="/Contact"
 							>
 								Contact
-							</a>
+							</Link>
 						</li>
 						<li>
-							<a
-								className="btn-ghost rounded-md p-2"
-								href="/Resume"
+							<Link
+								className={`btn-ghost rounded-md p-2 ${
+									currentPage === '/Resume' &&
+									'underline text-gray-300 font-medium'
+								}`}
+								to="/Resume"
 							>
 								Resume
-							</a>
+							</Link>
 						</li>
 					</ul>
 					<div className="dropdown dropdown-hover dropdown-end sm:hidden">
-						<div tabIndex={0} role="button" className="m-1 text-2xl btn-ghost p-2 rounded-xl">
+						<div
+							tabIndex={0}
+							role="button"
+							className="m-1 text-2xl btn-ghost p-2 rounded-xl"
+						>
 							<GiHamburgerMenu />
 						</div>
 						<ul
 							tabIndex={0}
-							className="dropdown-content menu bg-slate-600 rounded-box z-[1] w-52 p-2 shadow text-2xl"
+							className="dropdown-content menu bg-cyan-950 rounded-box z-[1] w-52 p-2 shadow text-2xl"
 						>
 							<li>
-							<a className="btn-ghost rounded-md p-2" href="/">
+							<Link
+								className={`btn-ghost rounded-md p-2 ${
+									currentPage === '/' &&
+									'underline text-gray-300 font-medium'
+								}`}
+								to="/"
+							>
 								About
-							</a>
+							</Link>
 						</li>
 						<li>
-							<a
-								className="btn-ghost rounded-md p-2"
-								href="/Portfolio"
+							<Link
+								className={`btn-ghost rounded-md p-2 ${
+									currentPage === '/Portfolio' &&
+									'underline text-gray-300 font-medium'
+								}`}
+								to="/Portfolio"
 							>
 								Portfolio
-							</a>
+							</Link>
 						</li>
 						<li>
-							<a
-								className="btn-ghost rounded-md p-2"
-								href="/Contact"
+							<Link
+								className={`btn-ghost rounded-md p-2 ${
+									currentPage === '/Contact' &&
+									'underline text-gray-300 font-medium'
+								}`}
+								to="/Contact"
 							>
 								Contact
-							</a>
+							</Link>
 						</li>
 						<li>
-							<a
-								className="btn-ghost rounded-md p-2"
-								href="/Resume"
+							<Link
+								className={`btn-ghost rounded-md p-2 ${
+									currentPage === '/Resume' &&
+									'underline text-gray-300 font-medium'
+								}`}
+								to="/Resume"
 							>
 								Resume
-							</a>
+							</Link>
 						</li>
 						</ul>
 					</div>
