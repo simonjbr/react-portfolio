62dd41dd0bb64ed8facd50779bd2e68293cd2771
Simon Brown <simonandon@live.com.au>
2024-06-29 11:18:53 +0800

    Added google font 'Teko'

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				teko: ['Teko', 'serif'],
+			},
+		},
 	},
 	plugins: [
 		// eslint-disable-next-line no-undef
