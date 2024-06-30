0de301828db032e6775e47be971012a4db1cb119
Simon Brown <simonandon@live.com.au>
2024-06-30 11:24:13 +0800

    Moved components into a div to get footer to stick to bottom of the page

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import './App.css';
 import { Outlet } from 'react-router-dom';
 
 import Header from './components/Header';
+import Footer from './components/Footer';
 // import About from './pages/About';
 // import Portfolio from './pages/Portfolio';
 // import Contact from './pages/Contact';
@@ -11,12 +12,11 @@ import Header from './components/Header';
 function App() {
 	return (
 		<>
-			<Header />
-			<Outlet />
-			{/* <About />
-			<Portfolio />
-			<Contact />
-			<Resume /> */}
+			<div className="min-h-screen">
+				<Header />
+				<Outlet />
+				<Footer />
+			</div>
 		</>
 	);
 }
