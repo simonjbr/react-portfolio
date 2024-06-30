9810d8d2d821437cfae579df77a38f842178899a
Simon Brown <simonandon@live.com.au>
2024-06-30 11:23:09 +0800

    Completed Footer component with icons and functioning links

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,24 @@
+import {
+	IoLogoGithub,
+	IoLogoLinkedin,
+	IoLogoStackoverflow,
+} from 'react-icons/io5';
+import { Link } from 'react-router-dom';
+
 export default function Footer() {
 	return (
 		<>
-			<footer></footer>
+			<div className="sticky top-full flex justify-evenly mb-5">
+				<Link to="https://github.com/simonjbr">
+					<IoLogoGithub className="text-5xl hover:text-gray-500 cursor-pointer" />
+				</Link>
+				<Link to="https://au.linkedin.com/">
+					<IoLogoLinkedin className="text-5xl hover:text-gray-500 cursor-pointer" />
+				</Link>
+				<Link to="https://stackoverflow.com/">
+					<IoLogoStackoverflow className="text-5xl hover:text-gray-500 cursor-pointer" />
+				</Link>
+			</div>
 		</>
 	);
 }
