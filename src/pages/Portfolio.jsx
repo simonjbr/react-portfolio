a347a42524486a54cae5fa7c52fd477c61fe229d
Simon Brown <simonandon@live.com.au>
2024-07-02 10:12:35 +0800

    Created array of project objects and rendered Project element for each of them. Added cropped images for each project.

--- src/pages/Portfolio.jsx
+++ src/pages/Portfolio.jsx
@@ -1,7 +1,54 @@
+import Project from '../components/Project';
+
 const Portfolio = () => {
+	const projects = [
+		{
+			id: 1,
+			title: 'PopChat',
+			gitHub: 'https://github.com/cawooda/triumvirate',
+			description:
+				'Node.js, Express, PostgreSQL, Sequelize, Bulma, Handlebars, Multer',
+			deploy: 'https://triumvirate.onrender.com',
+			image: 'src/assets/project-image-popchat-crop.png',
+		},
+		{
+			id: 2,
+			title: 'Boardgame Bliss',
+			gitHub: 'https://github.com/awesome-foursome/boardgame-bliss',
+			description: 'JavaScript, HTML, CSS, JQuery, Bulma',
+			deploy: 'https://awesome-foursome.github.io/boardgame-bliss/',
+			image: 'src/assets/project-image-boardgame-bliss-crop.png',
+		},
+		{
+			id: 3,
+			title: 'techBlog',
+			gitHub: 'https://github.com/simonjbr/tech-blog',
+			description: 'Node.js, Express, PostgreSQL, Sequelize, Handlebars',
+			deploy: 'https://tech-blog-v2l9.onrender.com/',
+			image: 'src/assets/project-image-techblog-crop.png',
+		},
+		{
+			id: 4,
+			title: 'vivoChat',
+			gitHub: 'TBA',
+			description: 'MERN Stack',
+			deploy: '',
+			image: 'src/assets/project-image-vivochat-crop.png',
+		},
+	];
+
 	return (
 		<>
-			<div>Portfolio</div>
+			<article className="pt-4 ps-4">
+				<h1 className="text-4xl underline">Portfolio</h1>
+				<div className="columns-2 max-sm:columns-1 my-12 px-6 gap-6">
+					{projects.map((project) => (
+						<div key={project.id} className="py-3 relative">
+							<Project {...project} />
+						</div>
+					))}
+				</div>
+			</article>
 		</>
 	);
 };
