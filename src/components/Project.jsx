c808755f755a30c426d036b9e79e83c98117f626
Simon Brown <simonandon@live.com.au>
2024-07-02 10:14:00 +0800

    Created and styled Project component using props from projects array.

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,7 +1,34 @@
-export default function Project() {
+import { IoLogoGithub } from 'react-icons/io5';
+import { Link } from 'react-router-dom';
+
+export default function Project(project) {
+	console.log(project);
+
 	return (
-		<>
-			<div></div>
-		</>
+		<div className="h-min  overflow-hidden group ">
+			<Link to={project.gitHub}>
+				<img
+					src={project.image}
+					alt={`${project.title} image`}
+					className="group-hover:blur-sm group-hover:brightness-50 group-hover:saturate-50 group-hover:scale-105 border-slate-600 transition-all duration-500 overflow-hidden"
+				/>
+				<div className="invisible group-hover:visible text-3xl absolute top-1/4 left-10">
+					<div className="">
+						<Link
+							to={project.deploy}
+							className="hover:text-white hover:text-4xl  transition-all duration-300"
+						>
+							{project.title}
+						</Link>
+						<Link to={project.gitHub}>
+							<IoLogoGithub className="hover:text-white hover:text-5xl  transition-all duration-300" />
+						</Link>
+					</div>
+					<p className="text-2xl hover:text-white hover:text-3xl  transition-all duration-300">
+						{project.description}
+					</p>
+				</div>
+			</Link>
+		</div>
 	);
 }
