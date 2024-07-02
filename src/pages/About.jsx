6a3b85813433787a837eafafbc42efd41ccb565a
Simon Brown <simonandon@live.com.au>
2024-07-02 10:43:06 +0800

    Styled About Me paragraph

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
 				<h1 className="text-4xl underline">About Me</h1>
 				<div className="">
 					<RxAvatar className="text-9xl my-12" />
-					<p>
+					<p className='text-xl px-4'>
 						The sun set over the tranquil lake, casting a warm,
 						golden glow across the water. Birds chirped softly in
 						the distance, their melodies blending harmoniously with
